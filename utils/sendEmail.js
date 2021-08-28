0a42620ae7620f85e397620399246788eb75876d
Jay Jhangiani <47749517+jaydjhangiani@users.noreply.github.com>
2021-08-28 11:19:48 +0530

    fixing bugs

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,24 +3,13 @@ const sgMail = require("@sendgrid/mail");
 const sendEmail = (options) => {
   sgMail.setApiKey(process.env.EMAIL_PASSWORD);
 
-  const templates = {
-    contactEmail: process.env.CONTACT_TEMPLATE_ID,
-    activationEmail: process.env.ACTIVATE_TEMPLATE_ID,
-    passwordResetEmail: process.env.RESET_TEMPLATE_ID,
-    expertRegistration: process.env.EXPERT_REGISTRATION_TEMPLATE_ID,
-    expertApproval: process.env.EXPERT_APPROVAL_TEMPLATE_ID,
-    expertRejection: process.env.EXPERT_REJECTION_TEMPLATE_ID,
-  };
-
   const msg = {
     to: options.to,
     from: process.env.EMAIL_FROM,
-    templateId: templates[options.templateName],
+    templateId: process.env.EMAIL_TEMPLATE,
     dynamic_template_data: {
-      firstName: options.firstName,
-      websiteURL: "https://help-releaf.web.app",
-      activationToken: options.activationToken,
-      resetToken: options.resetToken,
+      firstName: "JAy",
+      websiteURL: "https://jaydjhangiani.in",
     },
   };
 
