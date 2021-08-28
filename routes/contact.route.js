fd599ebd9524a7dd30634b8ac82d4f98931bcbf5
Jay Jhangiani <47749517+jaydjhangiani@users.noreply.github.com>
2021-08-28 11:19:08 +0530

    added contact route

---
+++ routes/contact.route.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { contact } = require("../controllers/contact.controller");
+
+router.route("/").post(contact);
+
+module.exports = router;
